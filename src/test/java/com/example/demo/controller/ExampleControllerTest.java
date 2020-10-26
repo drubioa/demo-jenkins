647963eeecd2dce911d82931d0ee20172a426bb5
Diego Rubio Abujas <dbiosag@gmail.com>
2020-10-27 00:11:41 +0100

    First commit

---
+++ src/test/java/com/example/demo/controller/ExampleControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ExampleControllerTest {
+
+    @Test
+    public void test1() {
+        // Do something
+    }
+
+    @Test
+    public void test2() {
+        // Do something
+    }
+}
